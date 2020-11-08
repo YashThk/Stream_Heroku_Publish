34c01c13b08276c3e5ee09061a1b1619f5444073
Yash Thakur <73901664+YashThk@users.noreply.github.com>
2020-11-07 19:07:59 -0500

    Add files via upload

---
+++ stream_heroku_publish_app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 21:38:01 2020
+
+@author: YshThk
+"""
+
+import streamlit as sl
+
+sl.title('Welcome to MA346: Data Science')
+sl.header('--------------------------------------------------------------')
+sl.success('Sadly, very little school maths focuses on how to win free drinks in a pub. - Matt Parker, Things to Make & D0 in the Fourth Dimension')
+sl.info('This is a brief on how to publish a web-app built in python using streamlit and heroku. Hope it is helpful.')
